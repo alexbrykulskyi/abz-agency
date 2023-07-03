9d29ae2b3e988f851abd05babe0a4e6ab4d8edb7
Alex <alex.brykulskyi@gmail.com>
2023-07-03 22:21:44 +0300

    init firebase

---
+++ firebase/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxsECzw7FTPXtQrsmI1agIu0-0q5gHRg4",
+  authDomain: "test-62be2.firebaseapp.com",
+  projectId: "test-62be2",
+  storageBucket: "test-62be2.appspot.com",
+  messagingSenderId: "901517756801",
+  appId: "1:901517756801:web:bdd7db52c940be59e14080",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
